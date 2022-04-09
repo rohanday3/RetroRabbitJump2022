87644d6780ea4a0473bf4e159b661e0fed987790
rohanday4@gmail.com <rohanday3>
2022-04-09 13:00:17 +0200

    redo

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Jump2022'`, () => {
+  it(`should have as title 'Jump2022_1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Jump2022');
+    expect(app.title).toEqual('Jump2022_1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Jump2022 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Jump2022_1 app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Jump2022';
+  title = 'Booker';
 }
